75b6b8fac8e708e4c0af208601030c0b52a3c9f9
chrislol <chrislol1998@gmail.com>
2024-11-24 14:58:34 +0800

    feature: rate

--- apps/zc/src/app/(zc)/docs/rate/page.tsx
+++ apps/zc/src/app/(zc)/docs/rate/page.tsx
@@ -1,5 +1,5 @@
 import { Rate } from '@/components/rate';
 
 export default function Page() {
-  return <Rate defaultValue={1.5}/>;
+  return <Rate defaultValue={1.5} precision={0.2} />;
 }
--- apps/zc/src/components/rate/rate.tsx
+++ apps/zc/src/components/rate/rate.tsx
@@ -4,25 +4,37 @@ import { Star } from 'lucide-react';
 import { useState } from 'react';
 import { useDefaultProps } from '@/hooks/use-default-props';
 import React from 'react';
-
 export interface RateProps {
   value?: number;
   defaultValue?: number;
   count?: number;
   icon?: React.ReactNode;
+  precision?: number;
+  disabled?: boolean;
 }
 export const Rate = (_props: RateProps) => {
   const props = useDefaultProps(_props, {
     defaultValue: 0,
     count: 5,
-    icon: <Star />,
+    icon: <Star color="#0858d9" />,
+    emptyIcon: <Star />,
+    precision: 1,
+    disabled: false,
   });
-  const { count, icon } = props;
+  const { count, icon, precision, disabled, emptyIcon } = props;
   const [value, setValue] = useControllableValue<RateProps['value']>(props);
-  const [hoverValue, setHoverValue] = useState(value);
+  const [hoverValue, setHoverValue] = useState(
+    updateValuePrecision(value ?? 0)
+  );
 
   function resetHoverValue() {
-    setHoverValue(value);
+    setHoverValue(updateValuePrecision(value ?? 0));
+  }
+
+  function updateValuePrecision(value: number) {
+    const impreciseValue = Math.floor(value / precision) * precision;
+
+    return parseFloat(impreciseValue.toFixed(1));
   }
 
   function getPreciseValue(event: React.MouseEvent, index: number) {
@@ -30,10 +42,11 @@ export const Rate = (_props: RateProps) => {
     const rect = element.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const ratio = parseFloat((x / rect.width).toFixed(1));
-    return index + ratio;
+    return index + updateValuePrecision(ratio);
   }
 
   function onClick(event: React.MouseEvent<HTMLDivElement>, index: number) {
+    if (disabled) return;
     const newValue = getPreciseValue(event, index);
     if (newValue === value) {
       setValue(0);
@@ -44,6 +57,7 @@ export const Rate = (_props: RateProps) => {
   }
 
   function onMouseMove(event: React.MouseEvent<HTMLDivElement>, index: number) {
+    if (disabled) return;
     setHoverValue(getPreciseValue(event, index));
   }
 
@@ -51,36 +65,36 @@ export const Rate = (_props: RateProps) => {
     if (hoverValue === undefined) return null;
     const fullIndex = Math.floor(hoverValue - 1);
     const hasDecimal = hoverValue % 1 !== 0;
-    const decimal = hasDecimal ? (hoverValue - fullIndex - 1).toFixed(1) : 0;
-    const commonIconProps = {
-      className: 'text-green-500',
-    };
+    const decimal = hasDecimal
+      ? updateValuePrecision(hoverValue) - Math.floor(hoverValue)
+      : 0;
 
     function renderIcon(i: number) {
       if (i <= fullIndex) {
-        return React.cloneElement(icon, { ...commonIconProps });
+        return React.cloneElement(icon);
       }
 
       if (i === fullIndex + 1 && hasDecimal) {
         return decimal;
       }
 
-      return React.cloneElement(icon);
+      return React.cloneElement(emptyIcon);
     }
 
     return [...Array(count)].map((_, i) => (
-      <div
-        key={i}
-        onClick={(e) => onClick(e, i)}
-        onMouseMove={(e) => onMouseMove(e, i)}
-      >
-        {renderIcon(i)}
+      <div key={i} className=" px-0.5">
+        <div
+          onClick={(e) => onClick(e, i)}
+          onMouseMove={(e) => onMouseMove(e, i)}
+        >
+          {renderIcon(i)}
+        </div>
       </div>
     ));
   }
 
   return (
-    <div className="flex" onMouseLeave={resetHoverValue}>
+    <div className="flex cursor-pointer" onMouseLeave={resetHoverValue}>
       {render()}
     </div>
   );
