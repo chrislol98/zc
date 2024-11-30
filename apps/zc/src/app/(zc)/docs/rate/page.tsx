0d4374525c1c67e15128009de94879c888182981
chrislol <chrislol1998@gmail.com>
2024-11-30 09:38:50 +0800

    feature: rate

--- apps/zc/src/app/(zc)/docs/rate/page.tsx
+++ apps/zc/src/app/(zc)/docs/rate/page.tsx
@@ -1,5 +1,32 @@
+'use client'
 import { Rate } from '@/components/rate';
+import { useState } from 'react';
 
 export default function Page() {
-  return <Rate defaultValue={1.5} precision={0.2} />;
+  // return <Rate defaultValue={1.5} precision={0.2} />;
+  return <App />;
+}
+
+function App() {
+  return (
+    <Parent>
+      <Child />
+    </Parent>
+  );
+}
+
+function Parent({ children }: { children: React.ReactNode }) {
+  const [value, setValue] = useState(0);
+  debugger;
+  return (
+    <div onClick={() => setValue((v) => v + 1)}>
+      {value}
+      {children}
+    </div>
+  );
+}
+
+function Child() {
+  console.log('child');
+  return 'child';
 }
